89980c91f6048073613b8570b371e2282618b627
Gallardo Lee <leegallardo16@gmail.com>
2024-01-14 15:08:34 +0800

    Create basic functions for calculator

---
+++ script.js
@@ -0,0 +1,30 @@
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+};
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+};
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+};
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+};
+
+let firstNumber, secondNumber, operator;
+
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case "+": 
+            return add(firstNumber,secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    };
+};
